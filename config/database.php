5d5d5be3e1e2549860bad36576ae6631565dcc8d
fernandolaurido00-rgb <fernandolaurido00@gmail.com>
2026-05-08 19:49:44 -0500

    fernando1

--- config/database.php
+++ config/database.php
@@ -8,11 +8,11 @@ return [
     // port: 3306
     // user: usuario MySQL creado en el panel
     // name: nombre exacto de tu base (Databases > MySQL)
-    'host' => 'mysql-novazu.alwaysdata.net',
+    'host' => ' ftp-laurido.alwaysdata.net',
     'port' => '3306',
-    'name' => 'novazu_geastiondespacho',
-    'user' => 'novazu',
-    'pass' => 'clase1234',
+    'name' => 'laurido_geastiondespacho',
+    'user' => 'laurdo',
+    'pass' => 'karen2216',
 
     // En true muestra detalle tecnico del error de conexion.
     'debug' => false,
